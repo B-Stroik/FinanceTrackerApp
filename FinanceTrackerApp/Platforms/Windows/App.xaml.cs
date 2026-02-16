4c59b3b93da6c983c0a1a6ddc80ef6ca779a32fc
Brandon Stroik <stroikbrandon@gmail.com>
2026-02-16 14:51:16 -0600

    Fixed windows compiling issues

--- FinanceTrackerApp/Platforms/Windows/App.xaml.cs
+++ FinanceTrackerApp/Platforms/Windows/App.xaml.cs
@@ -16,7 +16,7 @@ namespace FinanceTrackerApp.WinUI
         /// </summary>
         public App()
         {
-            this.InitializeComponent();
+            InitializeComponent();
         }
 
         protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
