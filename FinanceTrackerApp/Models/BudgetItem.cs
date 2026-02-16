2ee03fe22fb1f46e41d592474d16483738b11863
Brandon Stroik <stroikbrandon@gmail.com>
2026-02-16 12:51:22 -0600

    Added Model variables

--- FinanceTrackerApp/Models/BudgetItem.cs
+++ FinanceTrackerApp/Models/BudgetItem.cs
@@ -1,10 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using SQLite;
 
-namespace FinanceTrackerApp.Models
+namespace FinanceTracker.Models;
+
+public class BudgetItem
 {
-    internal class BudgetItem
-    {
-    }
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
+
+    public int Year { get; set; }
+    public int Month { get; set; }
+    public string Category { get; set; } = "";
+
+    public decimal MonthlyLimit { get; set; }
 }
--- FinanceTrackerApp/Models/TransactionItem.cs
+++ FinanceTrackerApp/Models/TransactionItem.cs
@@ -1,10 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using SQLite;
 
-namespace FinanceTrackerApp.Models
+namespace FinanceTracker.Models;
+
+public enum TransactionType
 {
-    internal class TransactionItem
-    {
-    }
+    Expense = 0,
+    Income = 1
+}
+
+public class TransactionItem
+{
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
+
+    public TransactionType Type { get; set; }
+
+    public decimal Amount { get; set; }
+    public string Category { get; set; } = "";
+    public DateTime Date { get; set; } = DateTime.Today;
+
+    public string? Note { get; set; }
 }
